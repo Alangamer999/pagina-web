53a54f088462f0f359637b26509cc2944386224f
TheGamers <alancn2026@gmail.com>
2026-01-16 22:41:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function mostrarMensaje() {
     alert("¡Hola! Esto es JavaScript en acción.");
 }
+// Ejemplo: Cambiar color al hacer clic
+const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        alert('Navegando a ' + link.textContent);
+    });
+});
